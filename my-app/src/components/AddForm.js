05e1137e29a968482abcb308c19b325ca2d3187b
devashrlucas <devashrlucas@gmail.com>
2020-06-15 14:34:19 -0400

    Working on 'AddForm' component.

---
+++ my-app/src/components/AddForm.js
@@ -0,0 +1,7 @@
+import React, { useState }  from 'react';
+
+const AddForm = () => {
+    const [tasks, setTasks] = useState([]);
+}
+
+export default AddForm;
--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AddForm from './AddForm';
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <AddForm />
     </div>
   );
 }
