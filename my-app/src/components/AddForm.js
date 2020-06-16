1f2d1a90a35e90baea2dcee4c502fa44a5f5d117
devashrlucas <devashrlucas@gmail.com>
2020-06-16 09:17:37 -0400

    Finished 'AddForm' component.

--- my-app/src/components/AddForm.js
+++ my-app/src/components/AddForm.js
@@ -1,7 +1,27 @@
 import React, { useState }  from 'react';
 
-const AddForm = () => {
-    const [tasks, setTasks] = useState([]);
-}
+const AddForm = ({ newTodo }) => {
+  const [text, setText] = useState("");
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        newTodo(text);
+        event.target.reset();
+      }}
+    >
+      <input
+        type="text"
+        name="new"
+        placeholder="Add todo"
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+        text={text}
+      ></input>
+    </form>
+  );
+};
 
 export default AddForm;
