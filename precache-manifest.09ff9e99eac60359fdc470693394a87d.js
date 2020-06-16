0224a440db74625c4b224e5acec0048196196577
devashrlucas <devashrlucas@gmail.com>
2020-06-16 11:20:15 -0400

    Updates

---
+++ precache-manifest.09ff9e99eac60359fdc470693394a87d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b753ecec13ca901a3aae63f27727438a",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "294c778633648bb9fa2d",
+    "url": "/todoapp/static/css/main.e755eb4c.chunk.css"
+  },
+  {
+    "revision": "c9d37cf2577ffd4f51a9",
+    "url": "/todoapp/static/js/2.afc64788.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.afc64788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294c778633648bb9fa2d",
+    "url": "/todoapp/static/js/main.219fed4f.chunk.js"
+  },
+  {
+    "revision": "67c65a7526cd4cc4b7e0",
+    "url": "/todoapp/static/js/runtime-main.5d279818.js"
+  }
+]);
